bfedfc581a1696acf83780eb643b708fada6a10a
Roberto Rodriguez <roberto@rodriguezartavia.com>
2021-01-14 07:41:08 -0600

    fc

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        "light-blue": colors.lightBlue,
+        teal: colors.teal,
+        cyan: colors.cyan,
+        rose: colors.rose,
+      },
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/line-clamp"),
+  ],
+};
